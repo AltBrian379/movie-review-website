dedc2675f042a4a5f7b0f9228e535797d5366651
altbrian379 <brianaltamirano.business@gmail.com>
2023-09-08 11:54:54 -0700

    Added npm env for backend evironments. Added API Boiler plate

--- home.js
+++ home.js
@@ -1,9 +1,24 @@
-require('dotenv').config();
 
-const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=5d3e3e5aa0a63f6e85a45adaf181e95a&page=1';
+
+const MOVIEDB_API_KEY = "5d3e3e5aa0a63f6e85a45adaf181e95a";
+console.log(MOVIEDB_API_KEY);
+const temp_movie_id = '605886';
+
+
+const APILINK = `https://api.themoviedb.org/3/movie/${temp_movie_id}?api_key=${MOVIEDB_API_KEY}`;
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
-const SEARCHAPI = `https://api.themoviedb.org/3/search/movie?&api_key=${process.env.MOVIEDB_API_KEY}&query=`;
+const SEARCHAPI = `https://api.themoviedb.org/3/search/movie?&api_key=${MOVIEDB_API_KEY}&query=`;
+
 
 
 const featured = document.getElementById('featured');
 
+returnFeaturedMovie(APILINK);
+
+function returnFeaturedMovie(url){
+    fetch(url).then(res => res.json())
+    .then(function(data){
+        console.log(data);
+
+    })
+}
