9f892125960c869cb30e3b96bd75af5341f17f65
altbrian379 <brianaltamirano.business@gmail.com>
2023-09-23 13:45:02 -0700

    First Successful front-end to backend GET. Also Implemented sequential execution of functions in home.js in order to first get info from OUR database then get mvie info from TMDB

--- client/home.js
+++ client/home.js
@@ -1,37 +1,30 @@
 
 const MOVIEDB_API_KEY = "5d3e3e5aa0a63f6e85a45adaf181e95a";
-console.log(MOVIEDB_API_KEY);
-let movie_id;
-
-//Step 1: Make connection to our local database and retrieve info 
-const APILINK_LOCAL = 'https://localhost:8000/api/v1/reviews/';
-
-
-
-
-
-
-
-
-const APILINK = `https://api.themoviedb.org/3/movie/${movie_id}?api_key=${MOVIEDB_API_KEY}`;
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
 const SEARCHAPI = `https://api.themoviedb.org/3/search/movie?&api_key=${MOVIEDB_API_KEY}&query=`;
+ const featured = document.getElementById('featured-review-link');
+//Step 1: Make connection to our local database and retrieve info 
+const APILINK_LOCAL = 'http://localhost:8000/api/v1/reviews';
+let movie_id;
 
 /*API LINLK for Our API */
+/*Call Back Test. By using this we ensure sequential execution of the functions. */
+returnFeaturedMovieData(APILINK_LOCAL + "/featured", function () {
+    console.log("This happens next.")
+    const APILINK = `https://api.themoviedb.org/3/movie/${movie_id}?api_key=${MOVIEDB_API_KEY}`;
+    returnFeaturedMovieData_TMDB(APILINK);
+})
+//returnFeaturedMovieData_TMDB(APILINK);
 
-const featured = document.getElementById('featured-review-link');
 
-returnFeaturedMovieData(APILINK_LOCAL + "featured/");
-
-/* Not Yet */
-/*returnFeaturedMovieData_TMDB(APILINK);*/
-
-function returnFeaturedMovieData(url){
-    fetch(url).then(res => res.json())
+// TODO REASEARCH ASYNC AWAIT
+async function returnFeaturedMovieData(url, callback){
+    await fetch(url).then(res => res.json())
     .then(function(data){
-        console.log(data);
-
+        console.log("This should happen first");
+        movie_id = data.movie_id;
     })
+    callback();
 }
 
 function returnFeaturedMovieData_TMDB(url){
@@ -53,7 +46,5 @@ function returnFeaturedMovieData_TMDB(url){
 
         featured.appendChild(image);
         featured.appendChild(title);
-
-
     }) 
 }
