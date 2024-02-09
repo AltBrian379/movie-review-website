34af5978594680d4d9a0756d1bff5bf0665e9d64
AltBrian379 <brianaltamirano.business@gmail.com>
2024-02-08 18:22:55 -0800

    rewrote returnFeaturedMovieData callback to encapsulate data only necessary for the function, and to build the anchor to for the HTML to have a dynamically built link dependant on the featured movie retreived.

--- client/home.js
+++ client/home.js
@@ -5,7 +5,7 @@ const SEARCHAPI = `https://api.themoviedb.org/3/search/movie?&api_key=${MOVIEDB_
 //Step 1: Make connection to our local database and retrieve info 
 const APILINK_MOVIE_REVIEWS = 'http://localhost:8000/api/v1/reviews';
 
-const featured = document.getElementById('featured-review-link');
+
 
 
 /*API LINLK for Our API */
@@ -15,27 +15,31 @@ const featured = document.getElementById('featured-review-link');
 
 returnFeaturedMovieData(APILINK_MOVIE_REVIEWS + "/featured", async function (movie_id) {
     console.log("This happens next.")
+        let db_poster;
+        let db_name
+        let db_release;
+        const featured = document.getElementById('featured-review');
     await fetch(`https://api.themoviedb.org/3/movie/${movie_id}?api_key=${MOVIEDB_API_KEY}`).then(res => res.json()).then(function(data) {
-        const db_poster = IMG_PATH + data.poster_path;
-        const db_name = data.title;
-        const db_release = data.release_date.slice(0,4)
-
-        const image = document.createElement('img');
-        image.setAttribute('alt','Featured Movie');
-
-        const title = document.createElement('h4');
-
-        //detail
-        image.src = db_poster;
-        title.innerHTML = db_name + " (" + db_release + ")";
-
-        featured.appendChild(image);
-        featured.appendChild(title);
-
+        
+        db_poster = IMG_PATH + data.poster_path;
+        db_name = data.title;
+        db_release = data.release_date.slice(0,4)
     })
-
-
-
+    //construct
+    const image = document.createElement('img');
+    image.setAttribute('alt','Featured Movie');
+    const title = document.createElement('h4');
+    const anchor = document.createElement('a');
+    anchor.setAttribute('href',`./review.html?id=${movie_id}`);
+    anchor.setAttribute('id','featured-review-link');
+      //detail
+    image.src = db_poster;
+    title.innerHTML = db_name + " (" + db_release + ")";
+    //build
+    console.log(image+ "i" + anchor + "i" + title + "o" + featured);
+    anchor.appendChild(image)
+    anchor.appendChild(title)
+    featured.appendChild(anchor)
 
     //returnFeaturedMovieData_TMDB(APILINK);
 })
