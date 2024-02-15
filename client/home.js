953d4331f78ebeff574b26dbe4472bd30efdd9f4
AltBrian379 <brianaltamirano.business@gmail.com>
2024-02-14 19:04:47 -0800

    Reverted port numbers to previous version

--- client/home.js
+++ client/home.js
@@ -3,7 +3,7 @@ const MOVIEDB_API_KEY = "5d3e3e5aa0a63f6e85a45adaf181e95a";
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
 const SEARCHAPI = `https://api.themoviedb.org/3/search/movie?&api_key=${MOVIEDB_API_KEY}&query=`;
 //Step 1: Make connection to our local database and retrieve info 
-const APILINK_MOVIE_REVIEWS = `http://localhost:${process.env.PORT}/api/v1/reviews`;
+const APILINK_MOVIE_REVIEWS = `http://localhost:8000/api/v1/reviews`;
 
 /*API LINK for Our API */
 /*Call Back Test. By using this we ensure sequential execution of the functions. */
