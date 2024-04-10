6e84dd594c059cb2d0fa230b4899e602fdbd31f8
AltBrian379 <brianaltamirano.business@gmail.com>
2024-04-09 19:28:30 -0700

    vercel modifications to routing, ports

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ console.log(mongo_password, mongo_username);
 const uri = `mongodb+srv://${mongo_username}:${mongo_password}@cluster0.zlmybst.mongodb.net/?retryWrites=true&w=majority`
 
 
-const port = 3000
+const port = 8000
 
 MongoClient.connect(
   uri,
